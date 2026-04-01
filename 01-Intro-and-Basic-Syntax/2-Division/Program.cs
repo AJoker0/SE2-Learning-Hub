4c20b863d5f19a09954145f07484d824cb9bcd4c
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-01 17:30:26 +0300

    3 and 4

--- 01-Intro-and-Basic-Syntax/2-Division/Program.cs
+++ 01-Intro-and-Basic-Syntax/2-Division/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+if ( number % 10 == 0)
+{
+    Console.WriteLine("The number is divisible by 10");
+} else if (number % 7 == 0)
+{
+    Console.WriteLine("The number is divisible by 7");
+} else if (number % 6 == 0)
+{
+    Console.WriteLine("The number is divisible by 6");
+} else if (number % 3 == 0)
+{
+    Console.WriteLine("The number is divisible by 3");
+} else if (number % 2 == 0)
+{
+    Console.WriteLine("The number is divisible by 2");
+} else
+{
+    Console.WriteLine("Not divisible");
+}
---
+++ 01-Intro-and-Basic-Syntax/3-Vacation/Program.cs
@@ -0,0 +1,70 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+string type = Console.ReadLine();
+string day = Console.ReadLine();
+
+double price = 0;
+double amount = 0;
+if (count > 0)
+{
+    if (type == "Students")
+    {
+        if (day == "Friday")
+        {
+            price = 8.45;
+        }
+        else if (day == "Saturday")
+        {
+            price = 9.80;
+        }
+        else if (day == "Sunday")
+        {
+            price = 10.46;
+        }
+    }
+    else if (type == "Business")
+    {
+        if (day == "Friday")
+        {
+            price = 10.90;
+        }
+        else if (day == "Saturday")
+        {
+            price = 15.60;
+        }
+        else if (day == "Sunday")
+        {
+            price = 16;
+        }
+    }
+    else if (type == "Regular")
+    {
+        if (day == "Friday")
+        {
+            price = 15;
+        }
+        else if (day == "Saturday")
+        {
+            price = 20;
+        }
+        else if (day == "Sunday")
+        {
+            price = 22.50;
+        }
+    }
+    amount = count * price;
+    if (type == "Students" && count >= 30)
+    {
+        amount *= 0.85;
+    }
+    else if (type == "Business" && count >= 100)
+    {
+        amount -= 10 * price;
+    }
+    else if (type == "Regular" && count >= 10 && count <= 20)
+    {
+        amount *= 0.95;
+    }
+}
+
+Console.WriteLine($"Total price: {amount:F2}");
