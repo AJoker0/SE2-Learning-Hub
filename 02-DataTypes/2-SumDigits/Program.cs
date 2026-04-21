689b767464f20b74c25c6407b3bc1bebd7977000
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-22 00:24:22 +0300

    starting before deadline

---
+++ 02-DataTypes/2-SumDigits/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int sum = 0;
+
+        foreach (char digit in input)
+        {
+            sum += digit - '0';
+        }
+        Console.WriteLine(sum);
+    }
+}
---
+++ 02-DataTypes/4-SumOfChars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            char ch = Console.ReadLine()[0];
+            sum += (int)ch;
+        }
+        Console.WriteLine($"The sum equals: {sum}");
+    }
+}
