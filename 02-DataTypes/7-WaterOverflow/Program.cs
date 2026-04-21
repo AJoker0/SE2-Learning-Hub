160839be516495bc03907507ec1e3e9808ef1b3a
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-22 00:50:24 +0300

    implement few from 2 sheet

--- 02-DataTypes/7-WaterOverflow/Program.cs
+++ 02-DataTypes/7-WaterOverflow/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int tank = 0;
+        int capacity = 255;
+
+        for (int i = 0; i < n; i++)
+        {
+            int liters = int.Parse(Console.ReadLine());
+
+            if (tank + liters > capacity)
+            {
+                Console.WriteLine("Insufficient capacity!");
+            } else
+            {
+                tank += liters;
+            }
+        }
+        Console.WriteLine(tank);
+
+    }
+}
---
+++ 02-DataTypes/8-BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string biggestModel = "";
+        double biggestVolume = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            string model = Console.ReadLine();
+            double radius = double.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double volume= Math.PI * radius * radius * height;
+
+            if (volume > biggestVolume)
+            {
+            biggestVolume = biggestVolume;
+            biggestModel = model;
+            }
+
+        }
+        Console.WriteLine(biggestModel);
+
+    }
+}
