582f09ab794f8951dbd991b20f9b1b21d47fb72f
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-22 12:42:21 +0300

    keep working with Arrays

--- 03-Arrays/5-TopIntegeers/Program.cs
+++ 03-Arrays/5-TopIntegeers/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] parts = Console.ReadLine().Split(' ');
+        int n = parts.Length;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = int.Parse(parts[i]);
+
+        List<int> tops = new List<int>();
+        int maxRight = int.MinValue;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr[i] > maxRight)
+            {
+                tops.Add(arr[i]);
+                maxRight = arr[i];
+            }
+        }
+
+        tops.Reverse();
+        Console.WriteLine(string.Join(" ", tops));
+    }
+}
