86bfcf01c25b2ae814013b0fbd5b0435147c6ed5
Andrii Titkov <andrii.titkov@gmail.com>
2026-04-22 11:35:31 +0300

    made task with pokemon

---
+++ 02-DataTypes/10-Pokemon/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+        long m = long.Parse(Console.ReadLine());
+        long y = long.Parse(Console.ReadLine());
+
+        long originalN = n;
+        int count = 0;
+
+        while (n >= m)
+        {
+            n -= m;
+            count++;
+
+            if (2 * n == originalN && y != 0)
+            {
+                n /= y;
+            }
+        }
+        Console.WriteLine(n);
+        Console.WriteLine(count);
+    }
+}
---
+++ 02-DataTypes/9-SpiceMustFlow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int yield = int.Parse(Console.ReadLine());
+        int totalSpice = 0;
+        int days = 0;
+
+        while (yield >= 100)
+        {
+            totalSpice += yield;
+            totalSpice -= 26; //workers eat 26
+            days++;
+            yield -= 10; //yield drops for next day 
+
+        }
+        totalSpice -= Math.Min(26, totalSpice);
+
+        Console.WriteLine(days);
+        Console.WriteLine(totalSpice);
+    }
+}
